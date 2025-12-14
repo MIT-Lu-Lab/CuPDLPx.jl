3d8912d4ea07de668482d8b32f51966b20d17cc9
Benoît Legat <benoit.legat@gmail.com>
2025-12-14 10:05:14 +0100

    Finish except attributes

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # cuPDLPx.jl
 Julia interface for cuPDLPx.
+
+## Use with JuMP
+
+To use cuPDLPx with JuMP, use `cuPDLPx.Optimizer`:
+
+```julia
+using JuMP, cuPDLPx
+model = Model(cuPDLPx.Optimizer)
+```
--- src/MOI_wrapper.jl
+++ src/MOI_wrapper.jl
@@ -5,21 +5,23 @@ MOI.Utilities.@product_of_sets(
     _LPProductOfSets,
     MOI.EqualTo{T},
     MOI.GreaterThan{T},
+    MOI.LessThan{T},
+    MOI.Interval{T},
 )
 
 const OptimizerCache = MOI.Utilities.GenericModel{
-    Float64,
-    MOI.Utilities.ObjectiveContainer{Float64},
-    MOI.Utilities.VariablesContainer{Float64},
+    Cdouble,
+    MOI.Utilities.ObjectiveContainer{Cdouble},
+    MOI.Utilities.VariablesContainer{Cdouble},
     MOI.Utilities.MatrixOfConstraints{
-        Float64,
+        Cdouble,
         MOI.Utilities.MutableSparseMatrixCSC{
-            Float64,
-            Int64,
-            MOI.Utilities.OneBasedIndexing,
+            Cdouble,
+            Cint,
+            MOI.Utilities.ZeroBasedIndexing,
         },
-        MOI.Utilities.Hyperrectangle{Float64},
-        _LPProductOfSets{Float64},
+        MOI.Utilities.Hyperrectangle{Cdouble},
+        _LPProductOfSets{Cdouble},
     },
 }
 
@@ -32,50 +34,19 @@ const BOUND_SETS = Union{
     MOI.Interval{Float64},
 }
 
-const ROW_SETS = Union{
-    MOI.EqualTo{Float64},
-    MOI.GreaterThan{Float64},
-}
-
 """
     Optimizer()
 
 Create a new cuPDLP optimizer.
 """
 mutable struct Optimizer <: MOI.AbstractOptimizer
-    output::Union{Nothing,SaddlePointOutput}
+    result::Union{Nothing,LibcuPDLPx.cupdlpx_result_t}
     max_sense::Bool
-    num_equalities::Int
-    silent::Bool
-    parameters::PdhgParameters
 
     function Optimizer()
         return new(
             nothing,
             false,
-	    0,
-            false,
-            PdhgParameters(
-                10,
-                false,
-                1.0,
-                1.0,
-                true,
-                2,
-                true,
-                64,
-                construct_termination_criteria(),
-                construct_restart_parameters(
-                    cuPDLP.ADAPTIVE_KKT,    # NO_RESTARTS FIXED_FREQUENCY ADAPTIVE_KKT
-                    cuPDLP.KKT_GREEDY,      # NO_RESTART_TO_CURRENT KKT_GREEDY
-                    1000,                   # restart_frequency_if_fixed
-                    0.36,                   # artificial_restart_threshold
-                    0.2,                    # sufficient_reduction_for_restart
-                    0.8,                    # necessary_reduction_for_restart
-                    0.5,                    # primal_weight_update_smoothing
-                ),
-                AdaptiveStepsizeParams(0.3, 0.6),
-            ),
         )
     end
 end
@@ -89,23 +60,25 @@ end
 # ====================
 
 function MOI.is_empty(optimizer::Optimizer)
-    return isnothing(optimizer.output)
+    return isnothing(optimizer.result)
 end
 
 function MOI.empty!(optimizer::Optimizer)
-    optimizer.output = nothing
+    optimizer.result = nothing
     return
 end
 
-MOI.get(::Optimizer, ::MOI.SolverName) = "cuPDLP"
+MOI.get(::Optimizer, ::MOI.SolverName) = "cuPDLPx"
 
 # MOI.RawOptimizerAttribute
 
 function MOI.supports(::Optimizer, param::MOI.RawOptimizerAttribute)
+    error("TODO")
     return hasfield(PdhgParameters, Symbol(param.name))
 end
 
 function MOI.set(optimizer::Optimizer, param::MOI.RawOptimizerAttribute, value)
+    error("TODO")
     if !MOI.supports(optimizer, param)
         throw(MOI.UnsupportedAttribute(param))
     end
@@ -114,6 +87,7 @@ function MOI.set(optimizer::Optimizer, param::MOI.RawOptimizerAttribute, value)
 end
 
 function MOI.get(optimizer::Optimizer, param::MOI.RawOptimizerAttribute)
+    error("TODO")
     if !MOI.supports(optimizer, param)
         throw(MOI.UnsupportedAttribute(param))
     end
@@ -125,6 +99,7 @@ end
 MOI.supports(::Optimizer, ::MOI.Silent) = true
 
 function MOI.set(optimizer::Optimizer, ::MOI.Silent, value::Bool)
+    error("TODO")
     optimizer.silent = value
     return
 end
@@ -146,7 +121,7 @@ end
 function MOI.supports_constraint(
     ::Optimizer,
     ::Type{MOI.ScalarAffineFunction{Float64}},
-    ::Type{<:ROW_SETS},
+    ::Type{<:BOUND_SETS},
 )
     return true
 end
@@ -169,35 +144,58 @@ function _flip_sense(optimizer::Optimizer, obj)
     return optimizer.max_sense ? -obj : obj
 end
 
+function sparse_matrix(A::MOI.Utilities.MutableSparseMatrixCSC{Cdouble,Cint,MOI.Utilities.ZeroBasedIndexing})
+    A_csc = LibcuPDLPx.MatrixCSC(
+        length(A.rowval),
+        pointer(A.colptr),
+        pointer(A.rowval),
+        pointer(A.nzval)
+    )
+
+    # 1. Allocate zeroed struct on Julia side
+    A_desc_ref = Ref{LibcuPDLPx.matrix_desc_t}()
+    A_desc_ref[] = LibcuPDLPx.matrix_desc_t(ntuple(_ -> UInt8(0), 56)) # Clear memory
+    A_desc_ptr = Base.unsafe_convert(Ptr{LibcuPDLPx.matrix_desc_t}, A_desc_ref)
+
+    # 2. Set Scalar Fields
+    A_desc_ptr.m = Cint(A.m)
+    A_desc_ptr.n = Cint(A.n)
+    A_desc_ptr.fmt = LibcuPDLPx.matrix_csc
+    A_desc_ptr.zero_tolerance = 0.0
+
+    # 3. Set The Union Data
+    A_desc_ptr.data.csc = A_csc
+
+    return A_desc_ptr
+end
+
 function MOI.optimize!(dest::Optimizer, src::OptimizerCache)
     MOI.empty!(dest)
-    Ab = src.constraints
-    A = convert(SparseMatrixCSC{Float64,Int64}, Ab.coefficients)
-    row_bounds = src.constraints.constants
     dest.max_sense = MOI.get(src, MOI.ObjectiveSense()) == MOI.MAX_SENSE
     obj = MOI.get(src, MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}())
-    c = zeros(A.n)
+    c = zeros(Cdouble, src.constraints.coefficients.n)
     for term in obj.terms
-        c[term.variable.value] += term.coefficient
+        c[term.variable.value] += _flip_sense(dest, term.coefficient)
     end
-    dest.num_equalities = MOI.get(src, MOI.NumberOfConstraints{MOI.ScalarAffineFunction{Float64},MOI.EqualTo{Float64}}())
-    prob = Lib.create_lp_problem(
+    obj_const = [_flip_sense(dest, MOI.constant(obj))]
+    prob = LibcuPDLPx.create_lp_problem(
         pointer(c),
-        A_desc_ptr,
-        pointer(con_lb),
-        pointer(con_ub),
-        pointer(var_lb),
-        pointer(var_ub),
+        sparse_matrix(src.constraints.coefficients),
+        pointer(src.constraints.constants.lower),
+        pointer(src.constraints.constants.upper),
+        pointer(src.variables.lower),
+        pointer(src.variables.upper),
         pointer(obj_const)
     )
-    if dest.silent
-        verbosity = dest.parameters.verbosity
-        dest.parameters.verbosity = 0
-    end
-    dest.output = unsafe_load(Lib.solve_lp_problem(prob, params_ptr))
-    if dest.silent # restore it
-        dest.parameters.verbosity = verbosity
-    end
+    @assert prob != C_NULL
+
+    params_ref = Ref{Lib.pdhg_parameters_t}()
+    Lib.set_default_parameters(Base.unsafe_convert(Ptr{Lib.pdhg_parameters_t}, params_ref))
+    params_ptr = Base.unsafe_convert(Ptr{Lib.pdhg_parameters_t}, params_ref)
+
+    result_ptr = Lib.solve_lp_problem(prob, params_ptr)
+    @assert result_ptr != C_NULL
+    dest.result = unsafe_load(result_ptr)
     return MOI.Utilities.identity_index_map(src), false
 end
 
@@ -209,64 +207,58 @@ function MOI.optimize!(dest::Optimizer, src::MOI.ModelLike)
 end
 
 function MOI.get(optimizer::Optimizer, ::MOI.SolveTimeSec)
-    return optimizer.output.iteration_status[end].cumulative_time_sec
+    return optimizer.result.cumulative_time_sec
 end
 
 function MOI.get(optimizer::Optimizer, ::MOI.RawStatusString)
-    if isnothing(optimizer.output)
+    if isnothing(optimizer.result)
         return "Optimize not called"
     else
-        return string(optimizer.output.termination_reason) * " : " * optimizer.output.termination_string
+        error("TODO")
     end
 end
 
 const _TERMINATION_STATUS_MAP = Dict(
-    TERMINATION_REASON_UNSPECIFIED => MOI.OPTIMIZE_NOT_CALLED,
-    TERMINATION_REASON_OPTIMAL => MOI.OPTIMAL,
-    TERMINATION_REASON_PRIMAL_INFEASIBLE => MOI.INFEASIBLE,
-    TERMINATION_REASON_DUAL_INFEASIBLE => MOI.DUAL_INFEASIBLE,
-    TERMINATION_REASON_TIME_LIMIT => MOI.TIME_LIMIT,
-    TERMINATION_REASON_ITERATION_LIMIT => MOI.ITERATION_LIMIT,
-    TERMINATION_REASON_KKT_MATRIX_PASS_LIMIT => MOI.NUMERICAL_ERROR,
-    TERMINATION_REASON_NUMERICAL_ERROR => MOI.NUMERICAL_ERROR,
-    TERMINATION_REASON_INVALID_PROBLEM => MOI.INVALID_MODEL,
-    TERMINATION_REASON_OTHER => MOI.OTHER_ERROR,
+    LibcuPDLPx.TERMINATION_REASON_UNSPECIFIED => MOI.OPTIMIZE_NOT_CALLED,
+    LibcuPDLPx.TERMINATION_REASON_OPTIMAL => MOI.OPTIMAL,
+    LibcuPDLPx.TERMINATION_REASON_PRIMAL_INFEASIBLE => MOI.INFEASIBLE,
+    LibcuPDLPx.TERMINATION_REASON_DUAL_INFEASIBLE => MOI.DUAL_INFEASIBLE,
+    LibcuPDLPx.TERMINATION_REASON_TIME_LIMIT => MOI.TIME_LIMIT,
+    LibcuPDLPx.TERMINATION_REASON_ITERATION_LIMIT => MOI.ITERATION_LIMIT,
+    LibcuPDLPx.TERMINATION_REASON_FEAS_POLISH_SUCCESS => MOI.OTHER_ERROR, # TODO
 )
 
 # Implements getter for result value and statuses
 function MOI.get(optimizer::Optimizer, ::MOI.TerminationStatus)
-    return isnothing(optimizer.output) ? MOI.OPTIMIZE_NOT_CALLED :
-           _TERMINATION_STATUS_MAP[optimizer.output.termination_reason]
+    return isnothing(optimizer.result) ? MOI.OPTIMIZE_NOT_CALLED :
+           _TERMINATION_STATUS_MAP[optimizer.result.termination_reason]
 end
 
 function MOI.get(optimizer::Optimizer, attr::MOI.ObjectiveValue)
     MOI.check_result_index_bounds(optimizer, attr)
-    return _flip_sense(optimizer, optimizer.output.iteration_stats[end].convergence_information[].primal_objective)
+    return _flip_sense(optimizer, optimizer.result.iteration_stats[end].convergence_information[].primal_objective)
 end
 
 function MOI.get(optimizer::Optimizer, attr::MOI.DualObjectiveValue)
     MOI.check_result_index_bounds(optimizer, attr)
-    return _flip_sense(optimizer, optimizer.output.iteration_stats[end].convergence_information[].dual_objective)
+    return _flip_sense(optimizer, optimizer.result.iteration_stats[end].convergence_information[].dual_objective)
 end
 
 const _PRIMAL_STATUS_MAP = Dict(
-    TERMINATION_REASON_UNSPECIFIED => MOI.NO_SOLUTION,
-    TERMINATION_REASON_OPTIMAL => MOI.FEASIBLE_POINT,
-    TERMINATION_REASON_PRIMAL_INFEASIBLE => MOI.NO_SOLUTION,
-    TERMINATION_REASON_DUAL_INFEASIBLE => MOI.INFEASIBILITY_CERTIFICATE,
-    TERMINATION_REASON_TIME_LIMIT => MOI.UNKNOWN_RESULT_STATUS,
-    TERMINATION_REASON_ITERATION_LIMIT => MOI.UNKNOWN_RESULT_STATUS,
-    TERMINATION_REASON_KKT_MATRIX_PASS_LIMIT => MOI.UNKNOWN_RESULT_STATUS,
-    TERMINATION_REASON_NUMERICAL_ERROR => MOI.UNKNOWN_RESULT_STATUS,
-    TERMINATION_REASON_INVALID_PROBLEM => MOI.UNKNOWN_RESULT_STATUS,
-    TERMINATION_REASON_OTHER => MOI.UNKNOWN_RESULT_STATUS,
+    LibcuPDLPx.TERMINATION_REASON_UNSPECIFIED => MOI.NO_SOLUTION,
+    LibcuPDLPx.TERMINATION_REASON_OPTIMAL => MOI.FEASIBLE_POINT,
+    LibcuPDLPx.TERMINATION_REASON_PRIMAL_INFEASIBLE => MOI.NO_SOLUTION,
+    LibcuPDLPx.TERMINATION_REASON_DUAL_INFEASIBLE => MOI.INFEASIBILITY_CERTIFICATE,
+    LibcuPDLPx.TERMINATION_REASON_TIME_LIMIT => MOI.UNKNOWN_RESULT_STATUS,
+    LibcuPDLPx.TERMINATION_REASON_ITERATION_LIMIT => MOI.UNKNOWN_RESULT_STATUS,
+    LibcuPDLPx.TERMINATION_REASON_FEAS_POLISH_SUCCESS => MOI.UNKNOWN_RESULT_STATUS,
 )
 
 function MOI.get(optimizer::Optimizer, attr::MOI.PrimalStatus)
     if attr.result_index > MOI.get(optimizer, MOI.ResultCount())
         return MOI.NO_SOLUTION
     end
-    return _PRIMAL_STATUS_MAP[optimizer.output.termination_reason]
+    return _PRIMAL_STATUS_MAP[optimizer.result.termination_reason]
 end
 
 function MOI.get(
@@ -275,27 +267,24 @@ function MOI.get(
     vi::MOI.VariableIndex,
 )
     MOI.check_result_index_bounds(optimizer, attr)
-    return optimizer.output.primal_solution[vi.value]
+    return optimizer.result.primal_solution[vi.value]
 end
 
 const _DUAL_STATUS_MAP = Dict(
-    TERMINATION_REASON_UNSPECIFIED => MOI.NO_SOLUTION,
-    TERMINATION_REASON_OPTIMAL => MOI.FEASIBLE_POINT,
-    TERMINATION_REASON_PRIMAL_INFEASIBLE => MOI.INFEASIBILITY_CERTIFICATE,
-    TERMINATION_REASON_DUAL_INFEASIBLE => MOI.NO_SOLUTION,
-    TERMINATION_REASON_TIME_LIMIT => MOI.UNKNOWN_RESULT_STATUS,
-    TERMINATION_REASON_ITERATION_LIMIT => MOI.UNKNOWN_RESULT_STATUS,
-    TERMINATION_REASON_KKT_MATRIX_PASS_LIMIT => MOI.UNKNOWN_RESULT_STATUS,
-    TERMINATION_REASON_NUMERICAL_ERROR => MOI.UNKNOWN_RESULT_STATUS,
-    TERMINATION_REASON_INVALID_PROBLEM => MOI.UNKNOWN_RESULT_STATUS,
-    TERMINATION_REASON_OTHER => MOI.UNKNOWN_RESULT_STATUS,
+    LibcuPDLPx.TERMINATION_REASON_UNSPECIFIED => MOI.NO_SOLUTION,
+    LibcuPDLPx.TERMINATION_REASON_OPTIMAL => MOI.FEASIBLE_POINT,
+    LibcuPDLPx.TERMINATION_REASON_PRIMAL_INFEASIBLE => MOI.INFEASIBILITY_CERTIFICATE,
+    LibcuPDLPx.TERMINATION_REASON_DUAL_INFEASIBLE => MOI.NO_SOLUTION,
+    LibcuPDLPx.TERMINATION_REASON_TIME_LIMIT => MOI.UNKNOWN_RESULT_STATUS,
+    LibcuPDLPx.TERMINATION_REASON_ITERATION_LIMIT => MOI.UNKNOWN_RESULT_STATUS,
+    LibcuPDLPx.TERMINATION_REASON_FEAS_POLISH_SUCCESS => MOI.UNKNOWN_RESULT_STATUS,
 )
 
 function MOI.get(optimizer::Optimizer, attr::MOI.DualStatus)
     if attr.result_index > MOI.get(optimizer, MOI.ResultCount())
         return MOI.NO_SOLUTION
     end
-    return _DUAL_STATUS_MAP[optimizer.output.termination_reason]
+    return _DUAL_STATUS_MAP[optimizer.result.termination_reason]
 end
 
 function MOI.get(
@@ -304,7 +293,7 @@ function MOI.get(
     ci::MOI.ConstraintIndex{MOI.ScalarAffineFunction{Float64},MOI.EqualTo{Float64}},
 )
     MOI.check_result_index_bounds(optimizer, attr)
-    return optimizer.output.dual_solution[ci.value]
+    return optimizer.result.dual_solution[ci.value]
 end
 
 function MOI.get(
@@ -313,11 +302,11 @@ function MOI.get(
     ci::MOI.ConstraintIndex{MOI.ScalarAffineFunction{Float64},MOI.GreaterThan{Float64}},
 )
     MOI.check_result_index_bounds(optimizer, attr)
-    return optimizer.output.dual_solution[optimizer.num_equalities + ci.value]
+    return optimizer.result.dual_solution[optimizer.num_equalities + ci.value]
 end
 
 function MOI.get(optimizer::Optimizer, ::MOI.ResultCount)
-    if isnothing(optimizer.output)
+    if isnothing(optimizer.result)
         return 0
     else
         return 1
